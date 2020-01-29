a2e6ee188d3d2138c59e61eb190b422877d87232
zeinatris <zhatris@hawkmail.hfcc.edu>
2020-01-29 04:05:53 -0500

    update hw1

--- HW/hw1.js
+++ HW/hw1.js
@@ -1,24 +1,33 @@
+//string
 let myName = "Zein Atris";
 let myMajor = "Web Developer";
 let myFunfact = "Creating websites";
 const myClass = 'CIS-172';
 
-
-
+//console.log
 console.log("Name:",myName);
 console.log("Major:",myMajor);
 console.log("Fun Fact:",myFunfact);
 console.log("I am using JS in",myClass);
 
+//for loop
 let a;
-for(a = 0; a <= 10; a++ )
+for(a = 10; a <= 20; a++ )
 {
     console.log("a:", a);
 }
 
+//while loop
 let counter = 10;
 while( counter > 0 )
 {
     counter--;
     console.log("Counter:", counter);
+}
+
+//if stament
+let name = "Mr.Banks"
+
+if (name == "Mr.Banks"){
+    console.log("Welcome back professor")
 }
