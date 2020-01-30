718b355bc0e31651373a509807856abaf453b498
zeinatris <zhatris@hawkmail.hfcc.edu>
2020-01-30 17:55:14 -0500

    update hw1

--- HW/hw1.js
+++ HW/hw1.js
@@ -10,13 +10,16 @@ console.log("Major:",myMajor);
 console.log("Fun Fact:",myFunfact);
 console.log("I am using JS in",myClass);
 
+//global scope
+var myEmail = ("zeinatris@yahoo.com");
+console.log("Email: ",myEmail)
+
 //for loop
 let a;
 for(a = 10; a <= 20; a++ )
 {
-    console.log("a:", a);
+    console.log("this is a for loop a: ", a);
 }
-
 //while loop
 let counter = 10;
 while( counter > 0 )
@@ -26,11 +29,11 @@ while( counter > 0 )
 }
 
 //if/else statement
-let name = "Mr.Banks"
+let name = "Mr.Banks";
 
-if (name == "Mr.Banks"){
-    console.log("Welcome back professor!")
+if (name === "Mr.Banks"){
+    console.log("Welcome back professor!");
 }
 else {
-    console.log("Welcome",myName,"what is your major?",myMajor)
+    console.log("Welcome",myName,"what is your major?",myMajor);
 }
