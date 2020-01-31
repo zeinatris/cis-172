e51f2a296c015088c0fe013f40f7ad17d5e02397
ZEINATRIS <ZEINATRIS@YAHOO.COM>
2020-01-30 19:32:25 -0500

    quiz 1

--- QUIZ/quiz1.js
+++ QUIZ/quiz1.js
@@ -7,9 +7,16 @@
  * @date        2020.01.30
  */
 
+// 1. Define a constant named favBook that holds the name of your favorite book.
+//      Also define a standard variables called favInt that contains your favorite int.
 const myfavBook = "good";
 
 let favInt = 42;
+
+// 2. Write an if statement that meets the following conditions.
+//      If favInt is greater than 42, console log the word BIGGER.
+//      If favInt is less than 42, console log the word SMALLER.
+//      If favInt is equal to 42, console log the word EQUAL.
  if (favInt > 42) {
      console.log("BIGGER")
  }
@@ -20,6 +27,7 @@ let favInt = 42;
      console.log("EQUAL")
  }
 
+ // 3. Write a for loop that iterates 7 times. Log the name of your book in each iteration.
 let c
 for(c = 0; c <= 7; c++ )
 {
@@ -31,6 +39,9 @@ for( let i in c )
     console.log( myfavBook[i] );
 }
 
+// 4. Write a while loop that iterates until favInt is less than or equal to -42.
+//      Inside the loop, write an if that will send a console error if favInt is 0.
+//      The console error should read "ZERO DETECTED!"
 
 while( favInt <= -42)
 {
