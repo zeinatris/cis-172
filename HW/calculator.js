04d72c991e5fa6d2b112dedcf6f606d6a4b636e3
zeinatris <zhatris@hawkmail.hfcc.edu>
2020-02-11 04:50:27 -0500

    calculator

--- HW/calculator.js
+++ HW/calculator.js
@@ -1,20 +1,59 @@
-function calc() {
-    var a = parseInt(document.querySelector("#value").value)
-    var op = parseInt(document.querySelector("#op").value)
-    var equal
+function number(num){
+    let a = document.getElementById('input');
+    switch(num){
+        case 1:
+            a.value += '1';
+            break;
+        case 2:
+            a.value += '2';
+            break;
+        case 3:
+            a.value += '3';
+            break;
+        case 4:
+            a.value += '4';
+            break;
+        case 5:
+            a.value += '5';
+            break;
+        case 6:
+            a.value += '6';
+            break;
+        case 7:
+            a.value += '7';
+            break;
+        case 8:
+            a.value += '8';
+            break;
+        case 9:
+            a.value += '9';
+            break;
+        case 0:
+            a.value += '0';
+            break;
+    }
+}
 
-    if(op === "badd") {
-        equal = a + a
+function oper(oper){
+    let a = document.getElementById('input');
+    switch(oper){
+        case '+':
+            a.value += '+';
+            break;
+        case '-':
+            a.value += '-';
+            break;
+        case '*':
+            a.value += '*';
+            break;
+        case '/':
+            a.value += '/';
+            break;
     }
-    else if(op === "bsub"){
-        equal = a - a
-    }
-    else if(op === "bmul"){
-        equal = a * a
-    }
-    else if(op === "bdiv"){
-        equal = a / a
-    }
+}
 
-    document.querySelector("#equal").innerHTML = equal
+function equal(){
+    let a = document.getElementById('input');
+    ans = Math.floor(+eval(a.value));
+    document.getElementById('input').value = '=' + ans;
 }
