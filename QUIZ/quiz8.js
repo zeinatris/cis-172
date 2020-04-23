c5d7e5785a9eedcbbdfcf0d8ad396ec9e2ca88e4
zeinatris <zhatris@hawkmail.hfcc.edu>
2020-04-23 18:44:40 -0400

    quiz 8

---
+++ QUIZ/quiz8.js
@@ -0,0 +1,33 @@
+/**
+* Quiz 8 focuses on Persistence
+*
+* @category    Quiz 8
+* @author      Chad Banks <crbanks1@hfcc.edu>
+* @author      Zein Atris <zhatris@hawkmail.hfcc.edu>
+* @date        2020.04.23
+* @grade
+*/
+
+// 2 pts
+// 1. Using JS or jQuery, create a variable called xp and use it to store the data-xp attribute value in the div below.
+// <div data-xp="95" ></div>
+let xp = document.getAttribute('data-xp');
+
+// 3 pts
+// 2. Create a localStorage key called plyrXp and store the value from the xp variable created above in it.
+let plyrXp = localStorage.getItem('xp');
+
+// 2 pts
+// 3. Pull the value for plyrXp out of localStorage, and store it in a variable called loadedXp.
+$('#data-xp').value = plyrXp;
+
+let loadedXp = document.getElementById('95');
+loadedXp.addEventListener('95',plyrXp).value;
+// 3 pts
+// 4. Create a cookie called "email" and store the string "crbanks1@hfcc.edu" in it, this cookie should expire in 30 days.
+let email = cookieManager.getCookies();
+cookieManager.setCookie("email", "cbanks1@hfcc.edu", 30);
+
+// 1 pt
+// E. Create a sessionStorage key called "session-page-visits" and store the number "42" in it.
+sessionStorage.setItem('session-page-visits', '42');
