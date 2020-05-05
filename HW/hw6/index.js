df4f8834a6596e70370eb431ce90129d52b3f5b7
Chad Banks <chadrbanks@yahoo.com>
2020-05-05 16:34:31 -0400

    HW 6

--- HW/hw6/index.js
+++ HW/hw6/index.js
@@ -1,29 +1,29 @@
-let johnDiv = $( '#JohnCena' );
+// Grade: 10 / 10
+
+let johnDiv = $("#JohnCena");
 let pageHandler = {
-    pageDiv: document.getElementById("myDiv"),
-    showBtn: document.getElementById("show"),
-    showBtn: addEventListener("click", johnDiv),
-    jQuery: document.getElementById("jq"),
+  pageDiv: document.getElementById("myDiv"),
+  showBtn: document.getElementById("show"),
+  showBtn: addEventListener("click", johnDiv),
+  jQuery: document.getElementById("jq"),
 
-    init: function(){
-
-        if (window.jQuery){
-            try{
-                console.log("The page has loaded!");
-            }
-            catch(e){
-                console.error("Error! Please refresh the page.", e);
-                throw new Error(pageHandler.init);
-            }
-        }
-        if (cenaDiv) {
-            cenaDiv = $('#myDiv');
-            $('#JohnCena').hide();
-        }
-        if (pageHandler.showBtn){
-            $('#JohnCena').show();
-        }
-        $('#jq').on( "click",pageHandler.jQuery );
+  init: function () {
+    if (window.jQuery) {
+      try {
+        console.log("The page has loaded!");
+      } catch (e) {
+        console.error("Error! Please refresh the page.", e);
+        throw new Error(pageHandler.init);
+      }
     }
-}
+    if (cenaDiv) {
+      cenaDiv = $("#myDiv");
+      $("#JohnCena").hide();
+    }
+    if (pageHandler.showBtn) {
+      $("#JohnCena").show();
+    }
+    $("#jq").on("click", pageHandler.jQuery);
+  },
+};
 window.onload = pageHandler.init;
